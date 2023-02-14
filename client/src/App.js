31d14a592f72cbe51986384608f45b7dac25ff2f
annkim7 <ilc7748@gmail.com>
2023-02-14 13:58:12 +0900

    #6 feat : 버튼 스타일 수정

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { increase, decrease } from './actions';
+import styled from 'styled-components';
+
+const Title = styled.h3`
+  color: #61dafb;
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+`;
+const Button = styled.button`
+  display: flex;
+  width: 4rem;
+  height: 4rem;
+  margin: 0.5rem;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem;
+  border: 0;
+  border-radius: 100%;
+  background: #61dafb;
+`;
 
 function App() {
   const state = useSelector((state) => state);
@@ -18,11 +39,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <section>
-          <h3>{`${state}`}</h3>
-          <div>
-            <button onClick={plusNum}>+</button>
-            <button onClick={minusNum}>-</button>
-          </div>
+          <Title>{`${state}`}</Title>
+          <ButtonBox>
+            <Button onClick={plusNum}>+</Button>
+            <Button onClick={minusNum}>-</Button>
+          </ButtonBox>
         </section>
       </header>
     </div>
